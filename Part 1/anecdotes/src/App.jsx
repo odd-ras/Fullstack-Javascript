e6eadf4e76df7684229c93aa1cd5110471eb40a6
odd-ras <knyakango@gmail.com>
2023-09-23 19:06:21 +0300

    final anecdotes exercises

---
+++ Part 1/anecdotes/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const arrayZeroFilled = new Array(anecdotes.length).fill(0);
+
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(arrayZeroFilled);
+  const [mostVotes, setMostVotes] = useState(0);
+
+  const handleRandomQuoteGeneration = () => {
+    const num = Math.floor(Math.random() * anecdotes.length);
+    setSelected(num);
+  };
+
+  const handleVotes = () => {
+    const newPoints = [...points];
+    newPoints[selected] += 1;
+    const highestVotes = Math.max(...newPoints);
+    setMostVotes(highestVotes);
+    setPoints(newPoints);
+  };
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>
+        {anecdotes[selected]}
+        <br /> has {points[selected]} votes
+      </p>
+      <button onClick={handleVotes}>vote</button>
+      <button onClick={handleRandomQuoteGeneration}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>
+        {anecdotes[points.indexOf(mostVotes)]} <br /> has {mostVotes} votes
+      </p>
+    </div>
+  );
+};
+
+export default App;
